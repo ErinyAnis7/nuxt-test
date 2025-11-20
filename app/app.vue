35cfd4ff8abe5314966b5392ad97635b69c5200e
Eriny Anis <E.Anis@tera.Itd>
2025-11-20 16:56:45 +0200

    add vuetifty

--- app/app.vue
+++ app/app.vue
@@ -18,4 +18,15 @@
   box-sizing: border-box;
   font-family: "Cairo", sans-serif;
 }
+
+.page-enter-active,
+.page-leave-active {
+  transition: 0.3s all ease;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  transform: scale(0, 0);
+  filter: blur(1rem);
+}
 </style>
